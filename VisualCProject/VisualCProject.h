5ef6fe72dd9b104be0e9083757f6382d6114300a
Raquel Almeida <raquel_soares@msn.com>
2012-11-09 10:48:52 -0800

    Initial Commit

---
+++ VisualCProject/VisualCProject.h
@@ -0,0 +1,13 @@
+// VisualCProject.h
+
+#pragma once
+
+using namespace System;
+
+namespace VisualCProject {
+
+	public ref class Class1
+	{
+		// TODO: Add your methods for this class here.
+	};
+}
